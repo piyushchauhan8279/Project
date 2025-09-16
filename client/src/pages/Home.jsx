5826eb5b39cb99e504bf4c1dcb7a4b98f72c3518
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-09-16 18:24:29 +0530

    add

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Testimonials from '../components/Testimonials'
+import Plan from '../components/Plan'
 const Home = () => {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
       <AiTools></AiTools>
+      <Testimonials/>
+      <Plan/>
     </>
   )
 }
