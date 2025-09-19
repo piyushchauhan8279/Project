d8de385ffd9c020bbd2b9f1d892770c399ec1336
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-09-19 13:36:31 +0530

    add

---
+++ client/src/components/Creations.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const Creations = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <div onClick={()=>setExpanded(!expanded)} className="p-4 border border-gray-200 rounded-lg cursor-pointer bg-white max-w-5xl text-sm">
+      <div className="flex gap-4 justify-between items-center">
+        <div>
+          <h2>{item.prompt}</h2>
+          <p className="text-gray-500">
+            {item.type}-{new Date(item.created_at).toLocaleDateString()}
+          </p>
+        </div>
+        <button className="bg-[#EFF6FF] border rounded-full px-4 py-1 border-[#BFDBFE] text-[#1E40AF]">
+          {item.type}
+        </button>
+      </div>
+      {expanded && (
+        <div>
+          {item.type == "image" ? (
+            <div>
+              <img
+                src={item.content}
+                className="mt-3 w-full max-w-md"
+                alt="image"
+              />
+            </div>
+          ) : (
+            <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-700">
+              {item.content}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Creations;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Gem, Sparkle } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import Creations from "../components/Creations";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h1>dashboard</h1>
-    </div>
-  )
-}
+  const [creations, setCreations] = useState([]);
 
-export default Dashboard
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+  return (
+    <div className="h-full p-6 overflow-y-scroll">
+      <div className="flex flex-wrap justify-start gap-4">
+        <div className="flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-lg font-semibold">
+              {dummyCreationData.length}
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] flex justify-center items-center">
+            <Sparkle className="w-5 text-white"></Sparkle>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center w-72 p-4 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-lg font-semibold">
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3C81F6] to-[#9234EA] flex justify-center items-center">
+            <Gem className="w-5 text-white"></Gem>
+          </div>
+        </div>
+      </div>
+
+      <div className="space-y-3 ">
+        <p className="mt-6 mb-4 ">Recent Creations</p>
+        {creations.map((item) => (
+          <Creations key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
