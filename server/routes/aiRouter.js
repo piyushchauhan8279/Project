ee49a10ab21aed7d3de215d546ad1e6120543122
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-09-29 13:51:49 +0530

    add

---
+++ server/routes/aiRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { auth } from "../middlewares/auth.js";
+import { generateArticle } from "../controllers/aicontroller.js";
+
+
+const aiRouter=express.Router();
+
+aiRouter.post('/write-article',auth,
+generateArticle)
+
+export default aiRouter
--- server/server.js
+++ server/server.js
@@ -4,14 +4,13 @@ import 'dotenv/config'
 
 
 import { clerkMiddleware,requireAuth } from '@clerk/express'
+import aiRouter from "./routes/aiRouter.js"
 
 const app=express()
 
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
-
-
 
 
 // routes
@@ -21,6 +20,7 @@ app.get('/',(req,res)=>{
 
 app.use(requireAuth())
 
+app.use('/api/ai',aiRouter)
 const PORT=process.env.PORT || 3000
 
 app.listen(PORT,()=>{
